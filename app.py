068ebc23a5f5e13c4ab9aa35a83ac317a76b4704
XimePss <ximepss@ciencias.unam.mx>
2019-11-12 12:25:20 -0600

    app actualizada

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 import numpy as np
+import pandas as pd
+import datetime as dt
 import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
@@ -6,16 +8,16 @@ from sqlalchemy import create_engine, func
 
 from flask import Flask, jsonify
 
-# Database setup
-engine = create_engine("sqlite:///Resources/hawaii.sqlite")
+engine = create_engine("sqlite:///./Resources/hawaii.sqlite")
 Base = automap_base()
 Base.prepare(engine, reflect=True)
+
+Base.classes.keys()
 
 Measurement = Base.classes.measurement
 Station = Base.classes.station
 
-# Flask
-app= Flask(__name__)
+app = Flask(_name_)
 
 @app.route("/")
 def welcome():
@@ -23,30 +25,62 @@ def welcome():
         Available routes:
         /api/v1.0/precipitation
         /api/v1.0/stations
-        /api/v1.0/tobs
     ''')
 
 @app.route("/api/v1.0/precipitation")
-def prep():
-    session=Session(engine)
-    result = session.query(Measurement.date,Measurement.prcp).all()
-    #aqui va el diccionario
-    session.close() 
+def prcps():
+    session = Session(engine)
+    results = session.query(Measurement.date, Measurement.prcp).all()
+session.close()
+    all_prcps = []
+    for date, prcp in results:
+        measurement_dic={}
+        measurement_dic["date"] = date
+        measurement_dic["prcp"] = prcp
+        all_prcps.append(measurement_dic)
+    return jsonify(all_prcps)
 
 @app.route("/api/v1.0/stations")
 def stat():
-    session=Session(engine)
-    result1 = session.query(Station.station).grouped_by(Station.station).all()
+    session = Session(engine)
+    results = session.query(Measurement.station).all()
     session.close()
-    return jsonify(result1)
+    all_stat = list(np.ravel(results))
+    return jsonify(all_stat)
 
 @app.route("/api/v1.0/tobs")
-def tobs():
-    session=Session(engine)
-    result2 = session.query(Measurements.date,Measurements.tobs).filter(Measurement.station=='USC00519281').filter(Measurement.date > '2016-08-18').all()
+def tempobs():
+    session = Session(engine)
+    results = session.query(Measurement.date, Measurement.tobs).\
+        filter(Measurement.date >= '2016-08-18').all()
     session.close()
-    return jsonify(result2)
+    all_tempobs = []
+    for date, tobs in results:
+        measurement_dic = {}
+        measurement_dic["date"] = date
+        measurement_dic["tobs"] = tobs
+        all_tempobs.append(measurement_dic)
+    return jsonify(all_tempobs)
 
+@app.route("/api/v1.0/<start>")
+def start_date(start):
+    print(start)
+    session = Session(engine)
+    results = session.query(Measurement.date, func.min(Measurement.tobs), func.max(Measurement.tobs), func.avg(Measurement.tobs)).\
+        filter(Measurement.date >= start).\
+        group_by(Measurement.date).all()       
+    session.close()
+    return jsonify({'results' : results})
 
-if __name__=="__main__":
+@app.route("/api/v1.0/<start>/<end>")
+def ranges(start, end):
+    session = Session(engine)
+    results = session.query(Measurement.date, func.min(Measurement.tobs), func.max(Measurement.tobs), func.avg(Measurement.tobs)).\
+        filter(Measurement.date >= start).\
+        filter(Measurement.date <= end).\
+        group_by(Measurement.date).all()
+    session.close()
+    return jsonify({'results' : results})
+
+if _name=="main_":
     app.run(debug=True)
